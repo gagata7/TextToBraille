386f75900de6ea14b270fea170b9c6eae903f991
agata <339102@uwr.edu.pl>
2025-10-17 00:10:17 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  cout<<"gata tu byla"<<endl;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  cout<<"waaa"<<endl;
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
